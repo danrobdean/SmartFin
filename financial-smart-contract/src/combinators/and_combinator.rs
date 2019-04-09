01779a9800875349fca09063d7c2cfdf392900e5
Daniel Dean <danrobdean@gmail.com>
2019-04-09 20:59:34 +0100

    Implemented combinator deserialization in constructor of smart contract.

--- financial-smart-contract/src/combinators/and_combinator.rs
+++ financial-smart-contract/src/combinators/and_combinator.rs
@@ -1,16 +1,19 @@
+extern crate pwasm_std;
+
 use super::contract_combinator::{ ContractCombinator, latest_horizon };
+use self::pwasm_std::Box;
 
 // The and combinator
-pub struct AndCombinator<'a, T: ContractCombinator, U: ContractCombinator> {
+pub struct AndCombinator {
     // The first sub-combinator
-    sub_combinator0: &'a T,
+    sub_combinator0: Box<ContractCombinator>,
     // The second sub-combinator
-    sub_combinator1: &'a U
+    sub_combinator1: Box<ContractCombinator>
 }
 
 // Method implementation for the and combinator
-impl<'a, T: ContractCombinator, U: ContractCombinator> AndCombinator<'a, T, U> {
-    pub fn new(sub_combinator0: &'a T, sub_combinator1: &'a U) -> AndCombinator<'a, T, U> {
+impl AndCombinator {
+    pub fn new(sub_combinator0: Box<ContractCombinator>, sub_combinator1: Box<ContractCombinator>) -> AndCombinator {
         AndCombinator {
             sub_combinator0,
             sub_combinator1
@@ -19,7 +22,7 @@ impl<'a, T: ContractCombinator, U: ContractCombinator> AndCombinator<'a, T, U> {
 }
 
 // Contract combinator implementation for the and combinator
-impl<'a, T: ContractCombinator, U: ContractCombinator> ContractCombinator for AndCombinator<'a, T, U> {
+impl ContractCombinator for AndCombinator {
     // Returns the latest of the two sub-horizons
     fn get_horizon(&self) -> Option<u64> {
         latest_horizon(self.sub_combinator0.get_horizon(), self.sub_combinator1.get_horizon())
--- financial-smart-contract/src/financial_sc.rs
+++ financial-smart-contract/src/financial_sc.rs
@@ -7,16 +7,18 @@ extern crate pwasm_abi_derive;
 
 use self::pwasm_std::types::{Address, U256};
 use self::pwasm_abi_derive::eth_abi;
-use self::pwasm_std::Box;
+use self::pwasm_std::{Box, Vec, vec};
 
 use combinators::contract_combinator::ContractCombinator;
+use combinators::zero_combinator::ZeroCombinator;
 use combinators::one_combinator::OneCombinator;
+use combinators::and_combinator::AndCombinator;
 
 // The financial smart contract interface
 #[eth_abi(FinancialScEndpoint)]
 pub trait FinancialScInterface {
     // The contract constructor
-    fn constructor(&mut self);
+    fn constructor(&mut self, contract_definition: Vec<u8>);
 
     // Gets the address of the counter-party
     #[constant]
@@ -29,25 +31,29 @@ pub trait FinancialScInterface {
 // The financial smart contract
 pub struct FinancialScContract {
     counter_party: Address,
-    combinator: Box<ContractCombinator>
+    combinator: Option<Box<ContractCombinator>>
 }
 
 // The financial smart contract interface implementation
 impl FinancialScInterface for FinancialScContract {
     // The financial smart contract constructor
-    fn constructor(&mut self) {
+    fn constructor(&mut self, contract_definition: Vec<u8>) {
         // Store the address of the counter-party
         self.counter_party = pwasm_ethereum::sender();
+        self.set_combinator(contract_definition);
     }
 
     // Gets the address of the counter-party
     fn get_counter_party(&mut self) -> Address {
-        return self.counter_party;
+        self.counter_party
     }
 
     // Gets the current value of the contract
     fn get_value(&mut self) -> U256 {
-        return self.combinator.acquire(pwasm_ethereum::timestamp()).into();
+        match self.combinator {
+            Some(ref combinator) => combinator.acquire(pwasm_ethereum::timestamp()).into(),
+            None => panic!("Attempted to get value before combinators initialised")
+        }
     }
 }
 
@@ -57,7 +63,31 @@ impl FinancialScContract {
     pub fn new() -> FinancialScContract {
         FinancialScContract{
             counter_party: Address::zero(),
-            combinator: Box::new(OneCombinator::new())
+            combinator: None
+        }
+    }
+
+    // Constructs the combinators from a serialized combinator contract
+    fn set_combinator(&mut self, serialized_combinators: Vec<u8>) {
+        let (_i, combinator) = FinancialScContract::deserialize_combinator(0, &serialized_combinators);
+        self.combinator = Some(combinator);
+    }
+
+    // Deserializes a combinator from the given combinator byte vector and index, returns the following index and the boxed combinator
+    fn deserialize_combinator(i: usize, serialized_combinators: &[u8]) -> (usize, Box<ContractCombinator>) {
+        if i > serialized_combinators.len() {
+            panic!("Provided combinator contract not valid.");
+        }
+
+        match serialized_combinators[i] {
+            0 => (i + 1, Box::new(ZeroCombinator::new())),
+            1 => (i + 1, Box::new(OneCombinator::new())),
+            2 => {
+                let (i0, sub_combinator0) = FinancialScContract::deserialize_combinator(i + 1, serialized_combinators);
+                let (i1, sub_combinator1) = FinancialScContract::deserialize_combinator(i0, serialized_combinators);
+                (i1, Box::new(AndCombinator::new(sub_combinator0, sub_combinator1)))
+            },
+            _ => panic!("Unrecognised combinator provided.")
         }
     }
 }
@@ -70,42 +100,89 @@ mod tests {
     use super::*;
     use self::pwasm_test::{ext_reset};
 
+    // Setup contract with default values, returns the contract
+    fn setup_contract(deserialized_combinator: Vec<u8>) -> FinancialScContract {
+        let sender = "1818909b947a9FA7f5Fe42b0DD1b2f9E9a4F903f".parse().unwrap();
+        let mut contract = FinancialScContract::new();
+
+        // Mock values
+        ext_reset(|e| e
+            .sender(sender)
+            .timestamp(0)
+        );
+        contract.constructor(deserialized_combinator);
+
+        contract
+    }
+
     // The counter-party of the contract is set to the deployer
     #[test]
     fn correct_counter_party() {
+        let sender = "1818909b947a9FA7f5Fe42b0DD1b2f9E9a4F903f".parse().unwrap();
         let mut contract = FinancialScContract::new();
 
-        // Mock sender address
-        let sender = "1818909b947a9FA7f5Fe42b0DD1b2f9E9a4F903f".parse().unwrap();
-        ext_reset(|e| e.sender(sender));
-
-        // Call contract functions
-        contract.constructor();
-        let counter_party = contract.get_counter_party();
+        // Mock values
+        ext_reset(|e| e
+            .sender(sender)
+        );
+        contract.constructor(vec![0]);
 
         // Check that counter-party is correct
+        let counter_party = contract.get_counter_party();
         assert_eq!(counter_party, sender);
     }
 
-    // The value of the contract is based on the given combinators
+    // The value of the contract is based on the given serialized combinator vector
     #[test]
-    fn correct_value() {
-        let mut contract = FinancialScContract::new();
-
-        // Mock blockchain details
-        let sender = "1818909b947a9FA7f5Fe42b0DD1b2f9E9a4F903f".parse().unwrap();
-        let timestamp = 0;
-        ext_reset(|e| e
-            .sender(sender)
-            .timestamp(timestamp)
-        );
-
-
-        // Call contract functions
-        contract.constructor();
-        let value: u64 = contract.get_value().low_u64();
+    fn correct_value_zero() {
+        let mut contract = setup_contract(vec![0]);
 
         // Check that the value is correct
+        let value: u64 = contract.get_value().low_u64();
+        assert_eq!(value, 0);
+    }
+
+    // The value of the contract is based on the given serialized combinator vector
+    #[test]
+    fn correct_value_one() {
+        let mut contract = setup_contract(vec![1]);
+
+        // Check that the value is correct
+        let value: u64 = contract.get_value().low_u64();
         assert_eq!(value, 1);
+    }
+
+    // The value of the contract is based on the given serialized combinator vector
+    #[test]
+    fn correct_value_and() {
+        let mut contract = setup_contract(vec![2, 1, 1]);
+
+        // Check that the value is correct
+        let value: u64 = contract.get_value().low_u64();
+        assert_eq!(value, 2);
+    }
+
+    // The value of the contract is based on the given serialized combinator vector
+    #[test]
+    fn contract_ignores_extra_combinators_in_serialized_vector() {
+        let mut contract = setup_contract(vec![0, 2, 1, 1]);
+
+        // Check that the value is correct
+        let value: u64 = contract.get_value().low_u64();
+        assert_eq!(value, 0);
+    }
+    
+    // Test that an empty deserialized combinator vector is not allowed
+    #[test]
+    #[should_panic]
+    fn should_panic_if_no_combinators_given() {
+        setup_contract(vec![]);
+    }
+
+    // Test that an undefined combinator vector value is not allowed
+    #[test]
+    #[should_panic]
+    fn should_panic_if_combinator_value_unrecognised() {
+        setup_contract(vec![255]);
     }
 }
