36bfc074526abc243820b6fa5814e66f9f24853b
Daniel Dean <danrobdean@gmail.com>
2019-04-29 19:13:23 +0100

    Changed to a promise structure for js tests, added stake tests.

--- financial-smart-contract/contract-js-test/test.js
+++ financial-smart-contract/contract-js-test/test.js
@@ -3,6 +3,11 @@ import { unlockAccount, loadAndDeployContract, serializeCombinatorContract } fro
 
 var contract;
 
+const uninvolved = {
+    address: "0x004ec07d2329997267ec62b4166639513386f32e",
+    password: "user"
+}
+
 const holder = {
     address: "0x057E231DaB35A789F5999056c8Ec775512609CBb",
     password: "test"
@@ -18,21 +23,23 @@ before(function(done) {
     // Unlock accounts
     unlockAccount(holder.address, holder.password).then(function(_) {
         unlockAccount(counterParty.address, counterParty.password).then(function(_) {
-            // Get serialized test contract
-            var combinatorContract = serializeCombinatorContract("one");
-
-            // First deployment may fail
-            loadAndDeployContract(combinatorContract, holder.address, counterParty.address).then(function(res) {
-                // First deployment succeeded
-                contract = res;
-                done();
-            }, function(_) {
-                // Should deploy successfully
+            unlockAccount(uninvolved.address, uninvolved.password).then(function(_) {
+                // Get serialized test contract
+                var combinatorContract = serializeCombinatorContract("one");
+    
+                // First deployment may fail
                 loadAndDeployContract(combinatorContract, holder.address, counterParty.address).then(function(res) {
+                    // First deployment succeeded
                     contract = res;
                     done();
-                }, function(err) {
-                    done(err);
+                }, function(_) {
+                    // Should deploy successfully
+                    loadAndDeployContract(combinatorContract, holder.address, counterParty.address).then(function(res) {
+                        contract = res;
+                        done();
+                    }, function(err) {
+                        done(err);
+                    });
                 });
             });
         });
@@ -40,25 +47,31 @@ before(function(done) {
 });
 
 describe('Contract tests', function() {
-    it('Returns the correct holder address', function(done) {
-        contract.methods.get_holder().call({ from: holder.address }, function(err, res) {
-            if (res) {
-                assert.equal(res.returnValue0, holder.address);
-                done();
-            } else {
-                done(err);
-            }
+    it('Returns the correct holder address', function() {
+        return contract.methods.get_holder().call({ from: uninvolved.address }).then(function(res) {
+            assert.equal(res.returnValue0, holder.address);
         });
     });
 
-    it('Returns the correct counter-party address', function(done) {
-        contract.methods.get_counter_party().call({ from: counterParty.address }, function(err, res) {
-            if (res) {
-                assert.equal(res.returnValue0, counterParty.address);
-                done();
-            } else {
-                done(err);
-            }
+    it('Returns the correct counter-party address', function() {
+        return contract.methods.get_counter_party().call({ from: uninvolved.address }).then(function(res) {
+            assert.equal(res.returnValue0, counterParty.address);
+        });
+    });
+
+    it('Updates stake correctly for the holder', function() {
+        return contract.methods.stake().send({ from: holder.address, value: 100 }).then(function() {
+            return contract.methods.get_balance().call({ from: holder.address }).then(function(res) {
+                assert.equal(res.returnValue0, 100);
+            });
+        });
+    });
+
+    it('Updates stake correctly for the counter-party', function() {
+        return contract.methods.stake().send({ from: counterParty.address, value: 100 }).then(function() {
+            return contract.methods.get_balance().call({ from: counterParty.address }).then(function(res) {
+                assert.equal(res.returnValue0, 100);
+            });
         });
     });
 })
